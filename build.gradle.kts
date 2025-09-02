b5f45768bf7f3e7f7ad65a6e0de9c3dad65fa705
Ghaylan Saada <ghaylan.saada@gmail.com>
2025-09-02 19:12:44 +0100

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,14 @@ kotlin {
 	compilerOptions {
 		javaParameters = true
 		jvmTarget.set(JvmTarget.JVM_21)
-		freeCompilerArgs.addAll("-Xjsr305=strict", "-opt-in=kotlin.RequiresOptIn", "-Xannotation-default-target=param-property")
+		freeCompilerArgs.addAll(
+            "-Xjsr305=strict",                       // strict nullability interop
+            "-java-parameters",                      // keep param names for Spring
+            "-Xjvm-default=all",                     // faster, cleaner proxies
+            "-Xemit-jvm-type-annotations",           // better interop for frameworks
+            "-opt-in=kotlin.RequiresOptIn",          // allow experimental APIs
+            "-Xstring-concat=indy-with-constants",   // best string concat on JDK 21
+            "-Xannotation-default-target=param-property")
 	}
 }
 
